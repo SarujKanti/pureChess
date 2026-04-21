5ff39feb187eb073597182ad62f00e7d76af195a
saruj <14skd12@gmail.com>
2026-04-21 16:04:01 +0530

    gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.skd.mychess"
+        applicationId = "skd.mychess"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
